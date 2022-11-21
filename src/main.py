ef2718496cdc28e14ef650601a92cf719f8c9f5f
Evantaur <jani.lehtimaki@spectralwolf.eu>
2022-11-21 17:47:40 +0200

    update main script with argparse

--- src/main.py
+++ src/main.py
@@ -1,40 +1,90 @@
 #!/usr/bin/env python3
 import sys
+import os
+import argparse
 from pathlib import Path
 
 __VERSION__=""
 
-user_help = f'''
-Generates a new file with a shebang based on file extention.
-
-Usage {sys.argv[0]} <inputfile>
-
-eg:
-{sys.argv[0]} python_project.py
-or
-{sys.argv[0]} {Path.home()}/python_project.py
 '''
+ARGPARSE
+'''
+parser = argparse.ArgumentParser(
+    description="Creates a new file with a template based on the file extension or template switch",
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    epilog=f'''
+Examples:
 
-#check if input
-if len(sys.argv) <= 1:
-    sys.exit(user_help)
+  # Creates a new file called test.sh
+  # with a shebang #!/bin/bash
 
-filename = sys.argv[1]
+  {sys.argv[0]} test.sh
+
+'''
+    )
 
 
+parser.add_argument(
+    '-d', '--dir',
+    help='Create directory structure if needed',
+    action="store_true"
+    )
 
+
+parser.add_argument(
+    '-v', '--version',
+    help='Show version and exit',
+    action='version',
+    version=f"%(prog)s version {__VERSION__}"
+    )
+
+
+parser.add_argument(
+    '-x',
+    help='Give execute permission (chmod +x)',
+    action="store_true"
+    )
+
+parser.add_argument('-t','--template',default=None, help='use this template')
+
+
+parser.add_argument('filename', help='filename')
+
+
+args = parser.parse_args()
+
+
+filename = args.filename
+
+
+#sys.exit(filename) #My god i'll have me shot if i push this thing with this fucking thing in it'
+
+
+#/!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\
+#!!! Do NOT modify manually, use build!!!
 #!TEMPLATES
+#!!! Do NOT modify manually, use build!!!
+#\!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!/
+
+
+
+
+
+
 
 #check if file exsists and quit if it does.
 if Path(filename).exists():
     sys.exit("File not empty")
 
-f_ext = Path(filename).suffix.strip('.')
+f_ext = Path(filename).suffix.strip('.') if not args.template else args.template
 sb = [""] if f_ext not in templates else templates[f_ext]
 try:
     with open(filename, 'w', encoding='utf-8') as file:
         # Writing data to a file
         file.writelines(sb)
         print(f"Wrote a new file to {filename}")
+        if args.x:
+            chmod = os.system(f"chmod +x {filename}")
+
 except FileNotFoundError:
 	print("No such directory.")
